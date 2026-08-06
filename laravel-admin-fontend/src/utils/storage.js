48e9dcf6475a9252bca655615b280c26494385fb
Sujoy Garai <sujoygaraikolkata@gmail.com>
2026-08-06 20:41:45 +0530

    Update to latest version

--- laravel-admin-fontend/src/utils/storage.js
+++ laravel-admin-fontend/src/utils/storage.js
@@ -1,15 +1,35 @@
+export const setToken = (accessToken, refreshToken) => {
+
+    localStorage.setItem(
+        "access_token",
+        accessToken
+    );
+
+    localStorage.setItem(
+        "refresh_token",
+        refreshToken
+    );
+
+};
+
+
+export const getToken = () => {
+    return localStorage.getItem("access_token");
+};
+
+
+export const getRefreshToken = () => {
+    return localStorage.getItem("refresh_token");
+};
 
 
 export const clearToken = () => {
 
     localStorage.removeItem("access_token");
-
     localStorage.removeItem("refresh_token");
 
     localStorage.removeItem("user");
-
     localStorage.removeItem("roles");
-
     localStorage.removeItem("permissions");
 
 };
